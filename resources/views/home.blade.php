da9223bcd83c55af12c9e1f0abdb8a3465ca8ac5
Igor-86 <i.campoli86@gmail.com>
2022-12-19 23:17:31 +0100

    More info in page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,23 @@
 
 @section('content')
     <div class="container">
-        <h1>Treni lista</h1>
+        <h2 class="mb-4">Treni</h2>
         <ul>
             @foreach ($trains as $item)
                 <li>
                     <h3>{{ $item->agency }}</h3>
+                    <div class="station">
+                        <p class="mb-0"> Stazione partenza: {{ $item->st_start }}</p>
+                        <p class="ms-3 mb-0"> Stazione arrivo: {{ $item->st_arrive }}</p>
+                        <div class="code ms-3">
+                            <p>Code</p>
+                            <p>{{ $item->code_train }}</p>
+                        </div>
+                        <p class="ms-3 mb-0"> Partenza: {{ $item->time_start }}</p>
+                        <p class="ms-3 mb-0"> Arrivo: {{ $item->time_arrive }}</p>
+                        <a class="btn btn-primary ms-3" href="#" role="button">More</a>
+
+                    </div>
                 </li>
             @endforeach
         </ul>
